d45dfb8d3f00d2a895e92c604e2247a0f00036ff
mateogalli <mateogalli@hotmail.com>
2025-04-18 18:21:54 -0300

    Usuarios de login

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,10 @@
+// Usuarios precargados
+const usuarios = {
+    bauti: "44243400",
+    mateo: "40475261",
+    alvaro: "43402735"
+};
+
 // Mostrar perfil del usuario logueado
 function mostrarPerfil() {
     const usuario = localStorage.getItem("usuarioLogueado");
@@ -11,7 +18,25 @@ function mostrarPerfil() {
     }
 }
 
-// Evento de envío del formulario
+// Login
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const usuario = document.getElementById("usuario").value;
+    const contraseña = document.getElementById("password").value;
+
+    if (usuarios[usuario] === contraseña) {
+        localStorage.setItem("usuarioLogueado", usuario);
+        document.getElementById("loginContainer").style.display = "none";
+        document.getElementById("formEstacionamiento").style.display = "inline-block";
+        document.querySelector("h1").style.display = "block";
+        mostrarPerfil();
+    } else {
+        document.getElementById("loginError").style.display = "block";
+    }
+});
+
+// Enviar datos del formulario
 document.getElementById("formEstacionamiento").addEventListener("submit", function(event) {
     event.preventDefault();
 
@@ -49,7 +74,9 @@ document.getElementById("formEstacionamiento").addEventListener("submit", functi
     });
 });
 
-// Mostrar el perfil al cargar la página
+// Al cargar la página
 window.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("formEstacionamiento").style.display = "none";
+    document.querySelector("h1").style.display = "none";
     mostrarPerfil();
 });
